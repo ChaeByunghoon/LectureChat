ca690ab48cd2ac44f49fecdbfb1182b3084fb330
ChaeByunghoon <cbh1203@naver.com>
2017-11-04 22:59:01 +0900

    액티비티 추가 및어뎁터 셋팅

---
+++ LectureChat/app/src/main/java/kr/co/hoonki/lecturechat/Board/QuestionActivity.java
@@ -0,0 +1,15 @@
+package kr.co.hoonki.lecturechat.Board;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.co.hoonki.lecturechat.R;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
